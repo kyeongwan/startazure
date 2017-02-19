05a249acf9594ca6d9dd8b963ed7a137ac1369a0
Byeongjin An <jugandotnet@outlook.com>
2017-02-19 22:16:02 +0900

    test.10

--- app.js
+++ app.js
@@ -1,58 +1,105 @@
-// 모듈을 추출합니다.
+var socketio = require('socket.io');
 
-var express = require('express');
+var express = require('express');
 
-var fs = require('fs');
+var http = require('http');
 
+var fs = require('fs');
 
+ 
 
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var seats = [       // 0 빈 공간, 1 예약가능 좌석, 2 예약이 완료된 좌석
 
-// 변수를 선언합니다.
-var seats = [
-    [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+
 ];
 
+ 
 
+//웹 서버 생성
 
-// 미들웨어를 설정합니다.
+var app = express();
+
+ 
+
+//미들웨어 설정
+
 app.use(app.router);
 
-// 라우트를 수행합니다.
-app.get('/', function (request, response, next) {
-    fs.readFile('inext.html', function (error, data) {
-        response.send(data.toString());
-    });
+ 
+
+//라우트 수행
+
+ 
+
+app.get('/', function (request, response, next) {
+
+    fs.readFile('index.html', function (error, data) {
+
+        response.send(data.toString());
+
+    });
+
 });
 
-app.get('/seats', function (request, response, next) {
-    response.send(seats);
+ 
+
+app.get('/seats', function (request, response, next) {
+
+    response.send(seats);
+
 });
 
+ 
 
+//웹서버를 실행
 
+var server = http.createServer(app)
 
-io.on('connection', function(socket){
- socket.on('reserve', function (data) {
-        seats[data.y][data.x] = 2;
-        io.sockets.emit('reserve', data);
-    });
+server.listen(52273, function () {
+
+    console.log('Server running at http://127.0.0.1:52273');
+
 });
 
-http.listen(port, function(){
- console.log('listening on ' + port);
+ 
+
+//소켓 서버를 생성 및 실행합니다.
+
+var io = socketio.listen(server);
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('reserve', function (data) {
+
+        
+
+        seats[data.y][data.x] = 2;
+
+        io.sockets.emit('reserve', data);
+
+    });
+
 });
