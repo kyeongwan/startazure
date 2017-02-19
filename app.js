6c1a2d316600120c0c764b501c6ae2d9098341e3
Byeongjin An <jugandotnet@outlook.com>
2017-02-19 22:11:37 +0900

    test.9

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // 모듈을 추출합니다.
-var socketio = require('socket.io');
+
 var express = require('express');
-var http = require('http');
+
 var fs = require('fs');
+
+
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 // 변수를 선언합니다.
 var seats = [
@@ -20,8 +27,7 @@ var seats = [
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
 ];
 
-// 웹 서버를 생성합니다.
-var app = express();
+
 
 // 미들웨어를 설정합니다.
 app.use(app.router);
@@ -37,17 +43,16 @@ app.get('/seats', function (request, response, next) {
     response.send(seats);
 });
 
-// 웹 서버를 실행합니다.
-var server = http.createServer(app)
-server.listen(52273, function () {
-    console.log('Server Running at http://127.0.0.1:52273');
-});
 
-// 소켓 서버를 생성 및 실행합니다.
-var io = socketio.listen(server);
-io.sockets.on('connection', function (socket) {
-    socket.on('reserve', function (data) {
+
+
+io.on('connection', function(socket){
+ socket.on('reserve', function (data) {
         seats[data.y][data.x] = 2;
         io.sockets.emit('reserve', data);
     });
+});
+
+http.listen(port, function(){
+ console.log('listening on ' + port);
 });
