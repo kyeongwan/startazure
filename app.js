eae7edb5d85126a11a0691d39f7c51b7ca2a1b33
Byeongjin An <jugandotnet@outlook.com>
2017-02-19 21:49:49 +0900

    test.3

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// 모듈 추출
+// 모듈을 추출합니다.
 var socketio = require('socket.io');
 var express = require('express');
 var http = require('http');
 var fs = require('fs');
 
-// 변수 선언.
+// 변수를 선언합니다.
 var seats = [
     [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
@@ -20,32 +20,30 @@ var seats = [
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
 ];
 
-// 웹 서버 생성
+// 웹 서버를 생성합니다.
 var app = express();
-var server = http.createServer(app);
 
-// 라우트 수행
+// 미들웨어를 설정합니다.
+app.use(app.router);
+
+// 라우트를 수행합니다.
 app.get('/', function (request, response, next) {
     fs.readFile('HTMLPage.html', function (error, data) {
         response.send(data.toString());
     });
 });
+
 app.get('/seats', function (request, response, next) {
     response.send(seats);
 });
 
- //웹 서버 실행
+// 웹 서버를 실행합니다.
+var server = http.createServer(app)
 server.listen(52273, function () {
     console.log('Server Running at http://127.0.0.1:52273');
 });
 
-//app.listen(process.env.port, function () {  //Updated
-//  var addr = app.address();
-//  console.log('   app listening on http://' + addr.address + ':' + addr.port);
-//});
-
-
-// 소켓 서버 생성 및 실행
+// 소켓 서버를 생성 및 실행합니다.
 var io = socketio.listen(server);
 io.sockets.on('connection', function (socket) {
     socket.on('reserve', function (data) {
