04da749d291a1853aa5f3e032a8b43acb6c0ad4c
Byeongjin An <jugandotnet@outlook.com>
2017-02-19 23:03:59 +0900

    test.14

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 
 var io = require('socket.io')(http);
- var express = require('express');
-var http = require('http').Server(app);
+var express = require('express');
+var http =  http.createServer(app)
 var fs = require('fs');
 var app = express();
-
-
 var port = process.env.PORT || 3000;
-
-
-
 
 var seats = [       // 0 빈 공간, 1 예약가능 좌석, 2 예약이 완료된 좌석
 
     [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
 
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
@@ -72,23 +62,17 @@ app.get('/seats', function (request, response, next) {
  // 웹 서버를 실행합니다.
 //var server = http.createServer(app)
 //server.listen(52273, function () {
-//    console.log('Server Running at http://127.0.0.1:52273');
+//console.log('Server Running at http://127.0.0.1:52273');
 //});
 
 
 //소켓 서버를 생성 및 실행합니다.
 io.on('connection', function(socket){
    socket.on('reserve', function (data) {
-
-        
-
-        seats[data.y][data.x] = 2;
-
-        io.sockets.emit('reserve', data);
+    seats[data.y][data.x] = 2;
+    io.sockets.emit('reserve', data);
  });
 });
-
-
 
 
 http.listen(port, function(){
